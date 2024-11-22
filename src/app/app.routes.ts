9c74802ecad5822d6eecb934e9d7dc087f350b17
IfSince <leonlaade@gmx.de>
2024-11-22 18:49:27 +0100

    add app layout with first dummy view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'upload'
+  },
+  {
+    path: 'upload',
+    loadComponent: () => import('./upload/upload-view/upload.view').then(c => c.UploadView),
+    title: 'Upload'
+  }
+];
---
+++ src/app/upload/upload-view/upload.view.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-upload-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload.view.html',
+})
+export class UploadView {
+}
