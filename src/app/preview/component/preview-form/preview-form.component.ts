4203de1d5a280b3eb23f2ae858e60447077f4df9
IfSince <leonlaade@gmx.de>
2025-02-18 02:25:10 +0100

    remove unused breakpoint control

--- src/app/preview/component/preview-form/preview-form.component.ts
+++ src/app/preview/component/preview-form/preview-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, inject, OnChanges, OnInit, Output, signal } from '@angular/core'
 import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card'
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { FormArray, FormGroup, ReactiveFormsModule } from '@angular/forms'
 import { MatButton } from '@angular/material/button'
 import { EditFormFieldComponent } from '../edit-form-field/edit-form-field.component'
 import { MaterialFieldComponent } from '../form-fields/material-field/material-field.component'
@@ -10,23 +10,11 @@ import { FieldType, FormField, MaterialComponentType } from '../../../formdata/m
 import { getFieldsAsFlatList } from '../../get-fields-as-flat-list'
 import { SelectableFormFieldComponent } from '../form-fields/selectable-form-field/selectable-form-field.component'
 import { MatIcon } from '@angular/material/icon'
-import { NgStyle } from '@angular/common'
 import { AbstractFormComponent } from '../../../common/component/abstract-form.component'
-import { MatFormField, MatLabel } from '@angular/material/form-field'
-import { MatOption } from '@angular/material/core'
-import { MatSelect } from '@angular/material/select'
 import { RouterLink } from '@angular/router'
 import { GetRawValuePipe } from '../../../common/get-raw-value.pipe'
 import { pairwise } from 'rxjs'
 import { toObservable } from '@angular/core/rxjs-interop'
-
-export const Breakpoints = [
-  { name: 'XSmall', width: '600px' },
-  { name: 'Small', width: '960px' },
-  { name: 'Medium', width: '1280px' },
-  { name: 'Large', width: '1920px' },
-  { name: 'Responsive', width: '100%' },
-]
 
 @Component({
   selector: 'app-preview-form',
@@ -45,12 +33,7 @@ export const Breakpoints = [
     SelectableFormFieldComponent,
     SelectableFormFieldComponent,
     MatIcon,
-    NgStyle,
     MatCardSubtitle,
-    MatFormField,
-    MatLabel,
-    MatOption,
-    MatSelect,
     RouterLink,
     GetRawValuePipe,
   ],
@@ -58,14 +41,12 @@ export const Breakpoints = [
   styleUrl: './preview-form.component.css',
 })
 export class PreviewFormComponent extends AbstractFormComponent<{ entries: FormField[] }> implements OnInit, OnChanges {
-  protected readonly Breakpoints = Breakpoints
   protected readonly FieldType = FieldType
   private formDataFormBuilderService = inject(FormDataFormBuilderService)
 
   @Output() select = new EventEmitter()
 
   _formGroup: FormGroup<{ entries: FormArray<FormGroup<ReactiveForm<FormField>>> }>
-  breakpointControl = new FormControl('100%')
 
   flattenedFields = signal<FormGroup<ReactiveForm<FormField>>[]>([])
   selectedField = signal<FormGroup<ReactiveForm<FormField>> | null>(null)
