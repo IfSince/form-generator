56e2fd425f6a2590e4dfcfbd25493a0ed53c802c
IfSince <leonlaade@gmx.de>
2025-02-26 13:34:37 +0100

    add hint icon to emphasize fields that were unsupported when creating form data

--- src/app/preview/component/form-fields/selectable-form-field/selectable-form-field.component.ts
+++ src/app/preview/component/form-fields/selectable-form-field/selectable-form-field.component.ts
@@ -1,18 +1,19 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core'
 import { NgClass } from '@angular/common'
-import { MatRipple } from '@angular/material/core'
+import { MatTooltip } from '@angular/material/tooltip'
 
 @Component({
   selector: 'app-selectable-form-field',
   standalone: true,
   imports: [
     NgClass,
-    MatRipple,
+    MatTooltip,
   ],
   templateUrl: './selectable-form-field.component.html',
   styleUrl: './selectable-form-field.component.css'
 })
 export class SelectableFormFieldComponent {
   @Input() selected = false
+  @Input() unsupported = false
   @Output() select = new EventEmitter<void>();
 }
